3bca7a40b25dbb99fa5b962d639b96eb097d13eb
Xin Geng <geng.xin@northeastern.edu>
2023-10-04 21:10:59 -0700

    complete assignment03

--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -9,7 +9,28 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 4)
+        self.assertEqual(items[0].quality, 11)
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 12, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 11)
+        self.assertEqual(items[0].quality, 21)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 5)
+        self.assertEqual(items[0].quality, 80)
 
 
 if __name__ == '__main__':
