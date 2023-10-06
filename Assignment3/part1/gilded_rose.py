0adca9f6f6575c49ad105f1570f7b0fc1e663c11
Xin Geng <geng.xin@northeastern.edu>
2023-10-05 19:29:40 -0700

    complete assignment03

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -21,14 +21,14 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name == "Sulfuras, Hand of Ragnaros":
+            if item.name == "Sulfuras":
                 continue
             if item.name == "Aged Brie":
                 if item.quality < 50:
                     item.quality += 1
                 if item.sell_in < 1 and item.quality < 50:
                     item.quality += 1
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            elif item.name == "Backstage passes":
                 if item.quality < 50:
                     item.quality += 1
                 if item.sell_in < 11 and item.quality < 50:
@@ -37,9 +37,24 @@ class GildedRose(object):
                     item.quality += 1
                 if item.sell_in < 1:
                     item.quality = 0
+            elif item.name == "Conjured":
+                if item.quality > 0:
+                    item.quality -= 2
+                if item.sell_in < 1 and item.quality > 0:
+                    item.quality -= 2
             else:
                 if item.quality > 0:
                     item.quality -= 1
                 if item.sell_in < 1 and item.quality > 0:
                     item.quality -= 1
             item.sell_in -= 1
+
+    def get_item_sell_in(self, name):
+        for item in self.items:
+            if item.name == name:
+                return item.sell_in
+
+    def get_item_quality(self, name):
+        for item in self.items:
+            if item.name == name:
+                return item.quality
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -6,31 +6,154 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", 5, 10)]
         gilded_rose = GildedRose(items)
+
+        original_sell_in = gilded_rose.get_item_sell_in("foo")
+        original_quality = gilded_rose.get_item_quality("foo")
         gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+        new_sell_in = gilded_rose.get_item_sell_in("foo")
+        new_quality = gilded_rose.get_item_quality("foo")
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality < original_quality
+        assert new_quality == original_quality - 1
 
     def test_aged_brie(self):
         items = [Item("Aged Brie", 5, 10)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(items[0].sell_in, 4)
-        self.assertEqual(items[0].quality, 11)
+        original_sell_in = gilded_rose.get_item_sell_in("Aged Brie")
+        original_quality = gilded_rose.get_item_quality("Aged Brie")
 
-    def test_backstage_passes(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 12, 20)]
-        gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(items[0].sell_in, 11)
-        self.assertEqual(items[0].quality, 21)
+        new_sell_in = gilded_rose.get_item_sell_in("Aged Brie")
+        new_quality = gilded_rose.get_item_quality("Aged Brie")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality > original_quality
+        assert new_quality == original_quality + 1
+
+    def test_backstage_passes_1(self):
+        items = [Item("Backstage passes", 10, 20)]
+        gilded_rose = GildedRose(items)
+
+        original_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        original_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        gilded_rose.update_quality()
+
+        new_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        new_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality > original_quality
+        assert new_quality == original_quality + 2
+
+    def test_backstage_passes_2(self):
+        items = [Item("Backstage passes", 5, 20)]
+        gilded_rose = GildedRose(items)
+
+        original_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        original_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        gilded_rose.update_quality()
+
+        new_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        new_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality > original_quality
+        assert new_quality == original_quality + 3
+
+    def test_backstage_passes_3(self):
+        items = [Item("Backstage passes", 0, 20)]
+        gilded_rose = GildedRose(items)
+
+        original_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        original_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        gilded_rose.update_quality()
+
+        new_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        new_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+        assert original_quality > 0
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality == 0
+
+    def test_backstage_passes_4(self):
+        items = [Item("Backstage passes", 15, 20)]
+        gilded_rose = GildedRose(items)
+
+        original_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        original_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        gilded_rose.update_quality()
+
+        new_sell_in = gilded_rose.get_item_sell_in("Backstage passes")
+        new_quality = gilded_rose.get_item_quality("Backstage passes")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality > original_quality
+        assert new_quality == original_quality + 1
 
     def test_sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
+        original_sell_in = gilded_rose.get_item_sell_in("Sulfuras")
+        original_quality = gilded_rose.get_item_quality("Sulfuras")
         gilded_rose.update_quality()
-        self.assertEqual(items[0].sell_in, 5)
-        self.assertEqual(items[0].quality, 80)
+        new_sell_in = gilded_rose.get_item_sell_in("Sulfuras")
+        new_quality = gilded_rose.get_item_quality("Sulfuras")
+
+        assert new_sell_in == original_sell_in
+        assert new_quality == original_quality
+        assert original_quality == 80
+
+    def test_conjured_item(self):
+        # Arrange
+        items = [Item("Conjured", 5, 42)]
+        gr = GildedRose(items)
+
+        original_sell_in = gr.get_item_sell_in("Conjured")
+        original_quality = gr.get_item_quality("Conjured")
+
+        # Act
+        gr.update_quality()
+
+        # Assert
+        new_sell_in = gr.get_item_sell_in("Conjured")
+        new_quality = gr.get_item_quality("Conjured")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality < original_quality
+        assert new_quality == original_quality - 2
 
 
 if __name__ == '__main__':
